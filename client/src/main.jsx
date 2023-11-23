a9b8c936d5a910f7cd25f85e1d4470995fb1c223
Ashish Kalaria <131858574+AshishKalaria@users.noreply.github.com>
2023-11-23 23:12:24 +0530

    some adjustments at front-end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 
 import customFetch from "./utils/customFetch.js";
 const data = await customFetch.get("/test");
@@ -10,6 +12,6 @@ console.log(data);
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<>
 		<App />
-		<ToastContainer position="top-center" />
+		<ToastContainer position="top-center" autoClose={2000} />
 	</>
 );
