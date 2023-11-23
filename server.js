6253a1d7d8bbbb53cd247eea664f99b0b7a3cc51
Ashish Kalaria <131858574+AshishKalaria@users.noreply.github.com>
2023-11-23 10:48:00 +0530

    little adjustments

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 import express from "express";
-const app = express();
 import morgan from "morgan";
 import * as dotenv from "dotenv";
-dotenv.config();
 import { nanoid } from "nanoid";
-import JobRouter from "./router/JobRouter";
+dotenv.config();
+const app = express();
 const port = process.env.PORT || 5100;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
 let jobs = [
 	{ id: nanoid(), company: "apple", position: "front-end" },
 	{ id: nanoid(), company: "google", position: "back-end" },
 ];
 
-app.use("/api/v1/jobs", JobRouter);
-
 app.get("/api/v1/jobs", (req, res) => {
 	res.status(200).json({ jobs });
 });
 
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
-}
-
-app.use(express.json());
-
 app.get("/", (req, res) => {
 	res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+	console.log(req);
+
+	res.json({ message: "Data received", data: req.body });
 });
 
 app.post("/api/v1/jobs", (req, res) => {
@@ -34,13 +38,11 @@ app.post("/api/v1/jobs", (req, res) => {
 		return res.status(400).json({ msg: "please provide company and position" });
 	}
 	const id = nanoid(10);
-	// console.log(id);
+	console.log(id);
 	const job = { id, company, position };
 	jobs.push(job);
 	res.status(200).json({ job });
 });
-
-// GET SINGLE JOB
 
 app.get("/api/v1/jobs/:id", (req, res) => {
 	const { id } = req.params;
@@ -50,8 +52,6 @@ app.get("/api/v1/jobs/:id", (req, res) => {
 	}
 	res.status(200).json({ job });
 });
-
-// EDIT JOB
 
 app.patch("/api/v1/jobs/:id", (req, res) => {
 	const { company, position } = req.body;
@@ -69,8 +69,6 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
 	res.status(200).json({ msg: "job modified", job });
 });
 
-// DELETE JOB
-
 app.delete("/api/v1/jobs/:id", (req, res) => {
 	const { id } = req.params;
 	const job = jobs.find((job) => job.id === id);
@@ -83,10 +81,6 @@ app.delete("/api/v1/jobs/:id", (req, res) => {
 	res.status(200).json({ msg: "job deleted" });
 });
 
-app.use("*", (req, res) => {
-	res.status(404).json({ msg: "not found" });
-});
-
-app.listen(port, () => {
-	console.log(`server running on PORT ${port}...`);
+app.listen(5100, () => {
+	console.log("server running....");
 });
